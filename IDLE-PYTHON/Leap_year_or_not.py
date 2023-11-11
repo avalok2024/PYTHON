f091b6c42accb28e4aa6957b447cb0d37aced6ea
minestream63 <av1662005@gmail.com>
2023-11-11 22:32:51 +0530

    leap year

---
+++ IDLE-PYTHON/Leap_year_or_not.py
@@ -0,0 +1,7 @@
+# Checking the given year is leap year or not?
+
+year = int(input("Enter the year  :"))
+if year%4 == 0:
+    print("This is a Leap year!")
+else: 
+    print("Oops! this is not a leap year")
