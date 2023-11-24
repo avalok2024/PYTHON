3935772efbe5b5d02e78c90df2d0c93019fcabd7
minestream63 <av1662005@gmail.com>
2023-11-24 19:25:05 +0530

    Update multiplicaiton_table.py

--- IDLE-PYTHON/multiplicaiton_table.py
+++ IDLE-PYTHON/multiplicaiton_table.py
@@ -4,10 +4,3 @@ n = int(input("Enter the number : "))
 
 for i in range(1 , 11):
     print(n , "x", i, "=" ,n*i) 
-
-# or
-
-i = 1 
-while i <= 10 :
-    print(n , "x" , i , "=", n*i)
-    i  += 1 
