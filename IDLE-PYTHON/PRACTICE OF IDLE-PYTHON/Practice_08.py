d8511a0c3bfdc494c9cdb26cd705fe66195ceb6b
minestream63 <av1662005@gmail.com>
2023-12-03 22:30:34 +0530

    22:30

--- IDLE-PYTHON/PRACTICE OF IDLE-PYTHON/Practice_08.py
+++ IDLE-PYTHON/PRACTICE OF IDLE-PYTHON/Practice_08.py
@@ -1,6 +1,26 @@
-rows = int(input("Enter The value of rows :" ))
+# Creating the quadratic equation solving python program 
 
-for i in range(rows, 0 ,-1): 
-    for j in range(1, i+1): 
-        print("*" ,end = "")
-        print()
+# importing the math moduel
+import math 
+
+# Taking the user from input 
+a = int(input("Enter the cofficent of x^2 : "))
+b = int(input("Enter the cofficent of x   : "))
+c = int(input("Enter the constant         : "))
+
+# Appling the Formula 
+
+d = b**2 - 4*a*c 
+
+# Using the if and else 
+
+if d > 0 :
+    x1 = (-b + math.sqrt(d))/(2*a)
+    x2 = (-b - math.sqrt(d))/(2*a)
+    print(f"The {x1} and {x2} are Real and Unequal roots")
+
+elif d == 0: 
+    x3 = -b/(2*a)
+    print(f"The {x3} is a real and equal roots")
+else:
+    print("No roots are here! ")
