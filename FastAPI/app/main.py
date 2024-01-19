a0353f8efaf06800880bf07341da6086673ed571
minestream63 <av1662005@gmail.com>
2024-01-19 23:19:27 +0530

    Complete C.R.U.D

--- FastAPI/app/main.py
+++ FastAPI/app/main.py
@@ -13,13 +13,19 @@ class Post(BaseModel):
     rating : Optional[int] = None
 
 my_posts = [{'title' :"This is post 1", "content":"content of post 1", 'id': 1},
-            {'title' : "Favroute foods ", "content": "I like pizza","id" : 2}]
+            {'title' : "Favroute foods ", "content": "I like pizza","id" : 2}, 
+            {"title": "this is new title ", "content" :"this is a new content", "id" : 3}]
 
  
 def find_post(id):
     for p in my_posts:
         if p['id'] == id:
             return p 
+
+def find_index_post(id):
+    for i, p in enumerate(my_posts):
+        if p['id'] == id:
+            return i
 
 
 @app.get("/")
@@ -55,4 +61,30 @@ def get_post(id: int):
         # Response.status_code = status.HTTP_404_NOT_FOUND
         # return {"message" : f"post with id : {id} was not found"}
     return {"post details": post}
+    
  
+@app.delete('/posts/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id : int):
+    # deleteing posts
+    # find the index in the array that has required ID 
+    # my_posts.pop(index) 
+    index = find_index_post(id)
+    if index == None:                                       
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail=f"post with id:{id} does not exits")
+    my_posts.pop(index)
+    return {"message" : "post was succesfully deleted"}
+    # return Response(status_code=status.HTTP_204_NO_CONTENT)
+    
+
+@app.put("/posts/{id}")
+def update_posts(id : int, post: Post):
+    index = find_index_post(id)
+
+    if index == None:  
+        raise HTTPException(status_code=status.HTTP_200_OK)
+
+    post_dict = post.dict()
+    post_dict['id'] = id
+    my_posts[index] = post_dict
+    return {"message": post_dict}   
